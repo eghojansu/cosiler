0858e43daed07739899206482f1474ee33158898
Eko Kurniawan <setiawanegho@gmail.com>
2022-02-04 16:10:06 +0700

    Add response json check

--- tests/unit/Http/ResponseTest.php
+++ tests/unit/Http/ResponseTest.php
@@ -148,4 +148,11 @@ final class ResponseTest extends \Codeception\Test\Unit
             $this->assertTrue(true);
         }
     }
+
+    public function testWantsJson()
+    {
+        $_SERVER['HTTP_ACCEPT'] = 'application/json';
+
+        $this->assertTrue(Response\wants_json());
+    }
 }
